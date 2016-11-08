b125ac48aeaf3873b2fea335f6cf1b2db8ceddfe
reoliveira <oliveira.reid@gmail.com>
2016-11-07 17:02:47 -0800

    new env variables setup compatable with bash

--- app/services/PublicConstants.java
+++ app/services/PublicConstants.java
@@ -3,4 +3,9 @@ package services;
 public class PublicConstants {
 
 	public static final String HTTP_GET = "GET";
+
+	public static final String TWITTER4J_CONSUMER_KEY = "twitter4j_consumerKey";
+	public static final String TWITTER4J_CONSUMER_SECRET = "twitter4j_consumerSecret";
+	public static final String TWITTER4J_ACCESSS_TOKEN = "twitter4j_accessToken";
+	public static final String TWITTER4J_ACCESS_TOKEN_SECRET = "twitter4j_accessTokenSecret";
 }
