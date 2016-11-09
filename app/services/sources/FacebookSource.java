2758d0406fe43178f36f65f84338c23ede96e20f
sammiej <jiangsammie@gmail.com>
2016-11-08 16:45:55 -0800

    Initial setup for Facebook4j

--- app/services/sources/FacebookSource.java
+++ app/services/sources/FacebookSource.java
@@ -1,18 +1,28 @@
 package services.sources;
 
+import services.dataAccess.proto.PostProto.Post;
+import facebook4j.Facebook;
+import facebook4j.FacebookException;
+import facebook4j.FacebookFactory;
+import facebook4j.Reading;
+import facebook4j.ResponseList;
+
+
 /**
- * Class representation of the Facebook RESTful API
+ * Class that interacts with the Facebook4j library to get data
  *
  * @author Reid Oliveira, Sammie Jiang
  */
-public class FacebookSource extends AbstractRestfulSource {
+public class FacebookSource extends AbstractJavaSource {
 	public static final String FACEBOOK = "facebook";
 	public static final String URL = "graph.facebook.com";
 	public static final String VERSION = "2.8";
 
+	Facebook facebook;
+
 	public FacebookSource() {
-		super(FACEBOOK, URL, VERSION);
+		super(FACEBOOK);
+		facebook = new FacebookFactory().getInstance();
 	}
 
-	// TODO methods should be representative of the facebook API and help construct API calls
 }
