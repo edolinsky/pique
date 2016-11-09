7b0e08dca97852a258b46494afbd84f7d62e1f94
MariaStrasky <maria.strasky@gmail.com>
2016-11-08 20:45:25 -0800

    Creating a post object for my test class

--- app/services/dataAccess/TestDataAccess.java
+++ app/services/dataAccess/TestDataAccess.java
@@ -18,6 +18,28 @@ public class TestDataAccess extends AbstractDataAccess {
   private Optional<Post> testOptPost;
   private Optional<PostList> testOptPostList;
 
+// For reference:
+
+/*  private class TestPost {
+      String id = "1";
+      String timestamp = "2";
+
+      String source = "3";
+      String source_link = "4";
+
+      int popularity_score = 5;
+      int popularity_velocity = 6;
+
+      int num_comments = 7;
+      int num_shares = 8;
+      int num_likes = 9;
+      String hashtag = "Hey";
+
+      String text = "This is the text in the post. Woohoo!";
+      String img_link = null;
+      String ext_link = null;
+  } */
+
   public long addNewPost(String keyString, Post post) {
     return x;
   }
@@ -39,6 +61,20 @@ public class TestDataAccess extends AbstractDataAccess {
   }
 
   public Optional<PostList> peekAtPostList(String keyString) {
-    return testOptPostList;
+    Post post = Post.newBuilder()
+      .setId("1")
+      .setTimestamp("2016-11-08 19:25:00")
+      .setPopularityScore(10)
+      .build();
+
+    PostList list = PostList.newBuilder()
+      .setPosts(0, post)
+      .build();
+
+    if (list == null) {
+        return Optional.empty();
+    } else {
+        return Optional.of(list);
+    }
   }
 }
